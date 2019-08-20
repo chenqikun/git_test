e35f7f668491235fce9307713d3b39d11ed935c2
chenqikun <qikunchen@foxemail.com>
2019-08-20 20:47:54 +0800

    添加tes工程到git

---
+++ git_test/src/main/java/com/ithema/test/Test.java
@@ -0,0 +1,10 @@
+package com.ithema.test;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("测试");
+        System.out.println("测试2");
+        System.out.println("测试3");
+        System.out.println("测试4");
+    }
+}
